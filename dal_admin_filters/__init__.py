f4d4c1e4cdd7eefd8c28e2bd0ac485fbd1f89ddb
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2024-09-18 02:33:48 +0300

    AutocompleteEmptyFieldFilter

--- dal_admin_filters/__init__.py
+++ dal_admin_filters/__init__.py
@@ -1,7 +1,8 @@
 # -*- encoding: utf-8 -*-
 from dal import autocomplete, forward
 from django import forms
-from django.contrib.admin.filters import ListFilter, SimpleListFilter, FieldListFilter
+from django.contrib.admin.filters import ListFilter, SimpleListFilter, FieldListFilter, EmptyFieldListFilter
+from django.db.models import Count
 from django.forms.widgets import Media, MEDIA_TYPES, media_property
 from django.contrib.admin.utils import get_fields_from_path
 
@@ -128,3 +129,73 @@ class AutocompleteFieldFilter(AutocompleteFilterMixin, FieldListFilter):
 
     def choices(self, changelist):
         return ()
+
+
+class AutocompleteEmptyFieldFilter(AutocompleteFilterMixin, EmptyFieldListFilter):
+    title_override: bool | str = True
+    null_override: bool | None = None
+    empty_strings_allowed_override: bool | None = None
+    empty: bool = True
+    count_distinct: bool = True
+
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        prev_field_null = field.null
+        if self.empty:
+            if self.null_override is not None:
+                field.null = self.null_override
+            if self.empty_strings_allowed_override is not None:
+                field.empty_strings_allowed = self.empty_strings_allowed_override
+        else:
+            field.null = True
+
+        self.field_name = field_path
+        super().__init__(field, request, params, model, model_admin, field_path)
+        self._init_autocomplete(request, params, model, model_admin)
+
+        if not self.empty:
+            field.null = prev_field_null
+
+        if isinstance(self.title_override, str):
+            self.title = self.title_override
+        elif self.title_override:
+            self.title = field_path.replace("__", " ").replace("_", " ")
+
+    def value(self):
+        val = self.used_parameters.get(self.parameter_name)
+        if isinstance(val, list):
+            assert len(val) == 1
+            return val[0]
+        return val
+
+    def get_queryset_for_field(self, model, name):
+        return self.get_queryset_for_field_obj(self.field)
+
+    def expected_parameters(self):
+        return [self.parameter_name, *super().expected_parameters()]
+
+    def queryset(self, request, queryset):
+        queryset = super().queryset(request, queryset)
+        if self.value():
+            queryset = queryset.filter(**{self.parameter_name: self.value()})
+        return queryset
+
+    def choices(self, changelist):
+        if not self.empty:
+            return ()
+
+        prev_get_query_string = changelist.get_query_string
+
+        def override_get_query_string(*args, **kwargs):
+            remove = list(kwargs.get("remove", None) or []) + [self.parameter_name]
+            kwargs["remove"] = remove
+            return prev_get_query_string(*args, **kwargs)
+
+        changelist.get_query_string = override_get_query_string
+        yield from super().choices(changelist)
+        changelist.get_query_string = prev_get_query_string
+
+    def get_facet_counts(self, pk_attname, filtered_qs):
+        counts_dict = super().get_facet_counts(pk_attname, filtered_qs)
+        for v in counts_dict.values():
+            v.distinct = self.count_distinct
+        return counts_dict
